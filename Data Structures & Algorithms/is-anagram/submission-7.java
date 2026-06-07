5e165bceb910af739f448dc5fc9bc69a89eafa6a
Dittakavi Nikhita <144314845+Nikhita-14@users.noreply.github.com>
2026-06-07 19:56:43 +0530

    Add: is-anagram - submission-7

---
+++ Data Structures & Algorithms/is-anagram/submission-7.java
@@ -0,0 +1,10 @@
+//1 Day Revision - Brute Force Sorting
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] sarr=s.toCharArray();
+        char[] tarr=t.toCharArray();
+        Arrays.sort(sarr);
+        Arrays.sort(tarr);
+        return Arrays.equals(sarr, tarr);
+    }
+}
