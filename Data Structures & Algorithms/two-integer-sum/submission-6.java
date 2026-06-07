8f5e6a0ae4f11795e62a88eb3a4c34700e7a9f9d
Dittakavi Nikhita <144314845+Nikhita-14@users.noreply.github.com>
2026-06-07 17:02:47 +0530

    Add: two-integer-sum - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum/submission-6.java
@@ -0,0 +1,33 @@
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] original = nums.clone();
+        Arrays.sort(nums);
+        int left=0;
+        int right=nums.length-1;
+        while(left<right){
+            if(nums[left]+nums[right]==target){
+                int l=linearSearchforward(original, nums[left]);
+                int j=linearSearchbackward(original, nums[right]);
+                return new int[]{Math.min(l,j), Math.max(l,j)};
+            }else if((nums[left]+nums[right])>target){
+                right--;
+            }else{
+                left++;
+            }
+        }
+        return new int[]{};
+    }
+    int linearSearchforward(int[] arr, int n){
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==n) return i;
+        }
+        return -1;
+    }
+    int linearSearchbackward(int[] arr, int n){
+        for(int i=arr.length-1;i>=0;i--){
+            if(arr[i]==n) return i;
+        }
+        return -1;
+    }
+}
