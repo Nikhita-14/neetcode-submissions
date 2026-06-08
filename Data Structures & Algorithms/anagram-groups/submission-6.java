6c77097c74bd6bdc952e8e1d465c72f9c25defd4
Dittakavi Nikhita <144314845+Nikhita-14@users.noreply.github.com>
2026-06-08 18:56:17 +0530

    Add: anagram-groups - submission-6

---
+++ Data Structures & Algorithms/anagram-groups/submission-6.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> hm = new HashMap<>();
+        for(String word : strs){
+            int[] count = new int[26];
+            for(char c : word.toCharArray()){
+                count[c-'a']++;
+            }
+            StringBuilder s = new StringBuilder();
+            for(int val : count){
+                s.append(val);
+                s.append('#');
+            }
+            String key = s.toString();
+            hm.putIfAbsent(key, new ArrayList<>());
+            hm.get(key).add(word);
+        }
+        return new ArrayList<>(hm.values());
+    }
+}
