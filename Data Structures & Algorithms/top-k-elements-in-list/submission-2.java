511d8bb4ae56795750f004da277a3484c018475b
Dittakavi Nikhita <144314845+Nikhita-14@users.noreply.github.com>
2026-06-13 22:22:25 +0530

    Add: top-k-elements-in-list - submission-2

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-2.java
@@ -0,0 +1,16 @@
+// Brute Force Approach - Frequency Sorting using Lambda Function T.C = O(nlogn) S.C = O(n)
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+       HashMap<Integer, Integer> hm = new HashMap<>();
+       for(int i=0;i<nums.length;i++){
+        hm.put(nums[i],hm.getOrDefault(nums[i],0)+1);
+       }
+       List<Integer> arr = new ArrayList<>(hm.keySet());
+       arr.sort((a,b)->hm.get(b)-hm.get(a));
+       int[] res = new int[k];
+       for(int i=0;i<k;i++){
+        res[i]=arr.get(i);
+       }
+       return res;
+    }
+}
